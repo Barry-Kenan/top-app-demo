01cf216f760770d6440e85a37d6be460af092635
unknown <bary.berdiyev.97@mail.ru>
2022-11-22 10:59:28 +0700

    настройка окружения

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export {default as Htag} from './Htag/Htag';
+export {default as Button} from './Button/Button';
+export {default as P} from './P/P';
+export {default as Tag} from './Tag/Tag';
+export {default as Rating} from './Rating/Rating';
+
