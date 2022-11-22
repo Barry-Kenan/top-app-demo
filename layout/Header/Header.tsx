7cb93f05caefca95b97252c4bc0d116160d4eefb
unknown <bary.berdiyev.97@mail.ru>
2022-11-22 11:02:46 +0700

    hoc layout

---
+++ layout/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HeaderProps } from './Header.props';
+import styles from './Header.module.css';
+import cn from 'classnames';
+
+const Header = ({ ...props }: HeaderProps): JSX.Element => {
+    return (
+        <div {...props}>
+            Header
+        </div>
+    );
+};
+
+export default Header;
