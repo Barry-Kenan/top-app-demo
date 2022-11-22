1ee8fc92d722fab2ec3d82faa96d94dc97b2e7f3
unknown <bary.berdiyev.97@mail.ru>
2022-11-22 11:00:30 +0700

    компонент button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Button.module.css';
+import cn from 'classnames';
+import { ButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
+
+const Button = ({ appearence, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
+    return (
+        <button className={cn(styles.button, className, {
+            [styles.primary]: appearence == 'primary',
+            [styles.ghost]: appearence == 'ghost',
+        })}
+            {...props}
+        >
+            {children}
+            {arrow !== 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow === 'down'
+            })} >
+                <ArrowIcon />
+            </span>}
+        </button>
+    );
+};
+
+export default Button;
