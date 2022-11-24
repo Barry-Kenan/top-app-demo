191bf8412629aa24dbe78266bbe52a05444f5c8b
unknown <bary.berdiyev.97@mail.ru>
2022-11-24 14:00:50 +0700

    экспорт компонент

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,7 @@ export {default as Button} from './Button/Button';
 export {default as P} from './P/P';
 export {default as Tag} from './Tag/Tag';
 export {default as Rating} from './Rating/Rating';
+export {default as Card} from './Card/Card';
+export {default as HhData} from './HhData/HhData';
+
 
